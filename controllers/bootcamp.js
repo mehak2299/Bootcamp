1da692a90e8d24f7129deab67b81298bb0f997ba
mehak2299 <kaurkavneet72@gmail.com>
2025-03-25 22:17:13 +0530

    Bootcamp

---
+++ controllers/bootcamp.js
@@ -0,0 +1,19 @@
+exports.getBootcamps=(req,res,next)=>{
+   res.status(200).json({success:true,msg:'Show all bootcamps',hello:req.hello});
+}
+exports.getBootcamp=(req,res,next)=>{
+    res.status(200).json({success:true,msg:`Show bootcamp ${req.params.id}`});
+ }
+exports.createBootcamp=(req,res,next)=>{
+    res.status(200).json({success:true,msg:'Create new bootcamp'});
+
+}
+exports.updateBootcamp=(req,res,next)=>{
+    res.status(200).json({success:true,msg:`Update bootcamp ${req.params.id}`});
+
+}
+
+exports.deleteBootcamp=(req,res,next)=>{
+    res.status(200).json({success:true,msg:`Delete bootcamp ${req.params.id}`});
+
+}
