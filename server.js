8f7654ba10198b7ae8ae8324caedaa899b7347f1
mehak2299 <kaurkavneet72@gmail.com>
2025-03-28 08:51:19 +0530

    Database

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv')
-//Route Files
 const bootcamps = require('./routes/bootcamps');
 const morgan=require('morgan')
+const colors=require('colors')
+const connectDB=require('./config/db')
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+connectDB();
 const app = express();
 const logger = require("./middleware/logger");
 if(process.env.NODE_ENV==='development')
@@ -16,4 +19,12 @@ if(process.env.NODE_ENV==='development')
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server=app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error ${err.message}`.red)
+    server.close(()=>{
+        process.exit(1);
+    })
+})
