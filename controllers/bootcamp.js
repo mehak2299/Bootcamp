cac20ea27a9e9fa8844c1b759e241185ec1806a4
mehak2299 <kaurkavneet72@gmail.com>
2025-03-29 17:57:00 +0530

    DB

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -5,6 +5,7 @@ exports.getBootcamp=(req,res,next)=>{
     res.status(200).json({success:true,msg:`Show bootcamp ${req.params.id}`});
  }
 exports.createBootcamp=(req,res,next)=>{
+    console.log(req.body)
     res.status(200).json({success:true,msg:'Create new bootcamp'});
 
 }
--- server.js
+++ server.js
@@ -14,17 +14,36 @@ if(process.env.NODE_ENV==='development')
 {
     app.use(morgan('dev'));
 }
+//Body parser
+app.use(express.json());
 
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 const server=app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise)=>{
     console.log(`Error ${err.message}`.red)
     server.close(()=>{
+        process.exit(1);
+    })
+})
+process.on('uncaughtException',(err)=>{
+    console.error(`UncaughtExpetion ${err}`.magenta.bold)
+    process.exit(1)
+})
+process.on('SIGTERM',()=>{
+    server.close(()=>{
+        console.log("Server colsed SIGERM")
+        process.exit(1);
+    })
+})
+process.on('SIGINT',()=>{
+    server.close(()=>{
+        console.log("Server colsed SIGINt")
         process.exit(1);
     })
 })
