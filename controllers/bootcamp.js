5e8274f1ba2264d97b5b28eb21c6f7f620759017
mehak2299 <kaurkavneet72@gmail.com>
2025-04-20 13:40:38 +0530

    MongoDbConnection

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -4,11 +4,18 @@ exports.getBootcamps=(req,res,next)=>{
 exports.getBootcamp=(req,res,next)=>{
     res.status(200).json({success:true,msg:`Show bootcamp ${req.params.id}`});
  }
-exports.createBootcamp=(req,res,next)=>{
-    console.log(req.body)
-    res.status(200).json({success:true,msg:'Create new bootcamp'});
-
+exports.createBootcamp=async(req,res,next)=>{
+    const bootcamp=await Bootcamp.create(req.body);
+    res.status(201).json({
+        success:true,
+        data:bootcamp
+    })
 }
+// exports.createBootcamp=(req,res,next)=>{
+//     console.log(req.body)
+//     res.status(200).json({success:true,msg:'Create new bootcamp'});
+
+// }
 exports.updateBootcamp=(req,res,next)=>{
     res.status(200).json({success:true,msg:`Update bootcamp ${req.params.id}`});
 
